6d92f637b6e066cc1a58193c32324690abb8a90a
Munir Ahmad <muneer986@hotmail.com>
2024-03-23 23:11:33 -0700

    first commit

---
+++ db/school.sql
@@ -0,0 +1,324 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2024 at 07:07 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `AID` int(11) NOT NULL,
+  `ANAME` varchar(150) NOT NULL,
+  `APASS` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`AID`, `ANAME`, `APASS`) VALUES
+(1, 'admin', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `CID` int(11) NOT NULL,
+  `CNAME` varchar(150) NOT NULL,
+  `CSEC` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`CID`, `CNAME`, `CSEC`) VALUES
+(1, 'VIII', 'B'),
+(2, 'III', 'C'),
+(5, 'V', 'A'),
+(6, 'III', 'B'),
+(7, 'VI', 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam`
+--
+
+CREATE TABLE `exam` (
+  `EID` int(11) NOT NULL,
+  `ENAME` varchar(150) NOT NULL,
+  `ETYPE` varchar(150) NOT NULL,
+  `EDATE` varchar(150) NOT NULL,
+  `SESSION` varchar(150) NOT NULL,
+  `CLASS` varchar(150) NOT NULL,
+  `SUB` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `exam`
+--
+
+INSERT INTO `exam` (`EID`, `ENAME`, `ETYPE`, `EDATE`, `SESSION`, `CLASS`, `SUB`) VALUES
+(1, 'Term Exam', 'II-Term', '22-03-2024', 'FN', 'VIII', 'Germany'),
+(3, 'Term Exam', 'II-Term', '22-03-2024', 'FN', 'III', 'English'),
+(4, 'Term Exam', 'II-Term', '22-03-2024', 'AN', 'V', 'Maths'),
+(5, 'Term Exam', 'III-Term', '22-03-2024', 'FN', 'VIII', 'Germany');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hclass`
+--
+
+CREATE TABLE `hclass` (
+  `HID` int(11) NOT NULL,
+  `TID` int(11) NOT NULL,
+  `CLA` varchar(150) NOT NULL,
+  `SEC` varchar(150) NOT NULL,
+  `SUB` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `hclass`
+--
+
+INSERT INTO `hclass` (`HID`, `TID`, `CLA`, `SEC`, `SUB`) VALUES
+(1, 1, 'VIII', 'A', 'English'),
+(4, 4, 'III', 'B', 'English'),
+(5, 4, 'VIII', 'B', 'Germany');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mark`
+--
+
+CREATE TABLE `mark` (
+  `MID` int(11) NOT NULL,
+  `REGNO` varchar(150) NOT NULL,
+  `SUB` varchar(150) NOT NULL,
+  `MARK` varchar(150) NOT NULL,
+  `TERM` varchar(150) NOT NULL,
+  `CLASS` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `mark`
+--
+
+INSERT INTO `mark` (`MID`, `REGNO`, `SUB`, `MARK`, `TERM`, `CLASS`) VALUES
+(1, 'S101', 'Germany', '95', 'I-Term', 'III'),
+(2, 'S101', 'English', '65', 'II-Term', 'III');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `TID` int(11) NOT NULL,
+  `TNAME` varchar(150) NOT NULL,
+  `TPASS` varchar(150) NOT NULL,
+  `QUAL` varchar(150) NOT NULL,
+  `SAL` varchar(150) NOT NULL,
+  `PNO` varchar(150) NOT NULL,
+  `MAIL` varchar(150) NOT NULL,
+  `PADDR` text NOT NULL,
+  `IMG` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`TID`, `TNAME`, `TPASS`, `QUAL`, `SAL`, `PNO`, `MAIL`, `PADDR`, `IMG`) VALUES
+(3, 'ram', '1234', 'MCA', '15000', '9999999999', 'ram@gmail.com', 'Salem', 'staff/2.jpg'),
+(4, 'Sam', '123', 'MBA', '15000', '9888888888', 'sam@gmail.com', 'salem', 'staff/3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `ID` int(11) NOT NULL,
+  `RNO` varchar(150) NOT NULL,
+  `NAME` varchar(150) NOT NULL,
+  `FNAME` varchar(150) NOT NULL,
+  `DOB` varchar(150) NOT NULL,
+  `GEN` varchar(150) NOT NULL,
+  `PHO` varchar(150) NOT NULL,
+  `MAIL` varchar(150) NOT NULL,
+  `ADDR` text NOT NULL,
+  `SCLASS` varchar(150) NOT NULL,
+  `SSEC` varchar(150) NOT NULL,
+  `SIMG` varchar(150) NOT NULL,
+  `TID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`ID`, `RNO`, `NAME`, `FNAME`, `DOB`, `GEN`, `PHO`, `MAIL`, `ADDR`, `SCLASS`, `SSEC`, `SIMG`, `TID`) VALUES
+(1, 'S101', 'Hind', 'Ram', '22-03-2024', 'Female', '70440174', 'ram@gmail.com', 'Salem', 'III', 'C', 'student/4.jpg', 1),
+(3, 'S102', 'Elssa', 'ram', '22-03-2024', 'Female', '70440174', 'ram@gmail.com', 'Salem', 'VIII', 'A', 'student/3.jpg', 4),
+(4, 'S103', 'Rafi', 'Karam', '22-03-2024', 'Male', '70440175', 'Rafi@gmail.com', 'Salem', 'V', 'C', 'student/3.jpg', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub`
+--
+
+CREATE TABLE `sub` (
+  `SID` int(11) NOT NULL,
+  `SNAME` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `sub`
+--
+
+INSERT INTO `sub` (`SID`, `SNAME`) VALUES
+(1, 'Germany'),
+(2, 'English'),
+(3, 'Maths'),
+(5, 'Social Science'),
+(6, 'Arabic');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`AID`);
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`CID`);
+
+--
+-- Indexes for table `exam`
+--
+ALTER TABLE `exam`
+  ADD PRIMARY KEY (`EID`);
+
+--
+-- Indexes for table `hclass`
+--
+ALTER TABLE `hclass`
+  ADD PRIMARY KEY (`HID`);
+
+--
+-- Indexes for table `mark`
+--
+ALTER TABLE `mark`
+  ADD PRIMARY KEY (`MID`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`TID`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `sub`
+--
+ALTER TABLE `sub`
+  ADD PRIMARY KEY (`SID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `AID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `CID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `exam`
+--
+ALTER TABLE `exam`
+  MODIFY `EID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `hclass`
+--
+ALTER TABLE `hclass`
+  MODIFY `HID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `mark`
+--
+ALTER TABLE `mark`
+  MODIFY `MID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `TID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `sub`
+--
+ALTER TABLE `sub`
+  MODIFY `SID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
